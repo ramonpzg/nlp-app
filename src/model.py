548820f7c568de7d3ce41e9eeeaaaa79b4f7fee7
Ramon Perez <ramonpzg@protonmail.com>
2023-01-30 15:23:02 +1100

    trained model added to dvc remote storage

--- src/model.py
+++ src/model.py
@@ -3,8 +3,13 @@ import torch.nn as nn
 import pytorch_lightning as pl
 import torch.nn.functional as F
 from transformers import AutoModelForSequenceClassification
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 import torchmetrics
+import wandb
+import numpy as np
+
 
 class EOIRModel(pl.LightningModule):
     def __init__(self, model_name="google/bert_uncased_L-2_H-128_A-2", lr=1e-2):
@@ -12,27 +17,30 @@ class EOIRModel(pl.LightningModule):
         self.save_hyperparameters()
 
         self.bert = AutoModelForSequenceClassification.from_pretrained(
-            model_name, num_labels=2
-        )
+            model_name, num_labels=2, #output_hidden_states=True
+        )#.cuda()
+
         self.num_classes = 2
-        self.train_accuracy_metric = torchmetrics.Accuracy()
-        self.val_accuracy_metric = torchmetrics.Accuracy()
-        self.f1_metric = torchmetrics.F1(num_classes=self.num_classes)
+        self.train_accuracy_metric = torchmetrics.Accuracy(task="binary")
+        self.val_accuracy_metric = torchmetrics.Accuracy(task="binary")
+        self.f1_metric = torchmetrics.F1Score(task="binary")
         self.precision_macro_metric = torchmetrics.Precision(
-            average="macro", num_classes=self.num_classes
+            task="binary", average="macro", num_classes=self.num_classes
         )
         self.recall_macro_metric = torchmetrics.Recall(
-            average="macro", num_classes=self.num_classes
+            task="binary", average="macro", num_classes=self.num_classes
         )
-        self.precision_micro_metric = torchmetrics.Precision(average="micro")
-        self.recall_micro_metric = torchmetrics.Recall(average="micro")
+        self.precision_micro_metric = torchmetrics.Precision(task="binary", average="micro")
+        self.recall_micro_metric = torchmetrics.Recall(task="binary", average="micro")
 
-    def forward(self, input_ids, attention_mask):
-        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+    def forward(self, input_ids, attention_mask, labels=None):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
 
-        h_cls = outputs.last_hidden_state[:, 0]
-        logits = self.W(h_cls)
-        return logits
+        # h_cls = outputs.last_hidden_state#[:, 0]
+        # h_cls = outputs.hidden_states[0]
+        # logits = self.W(h_cls)
+        # return logits
+        return outputs
 
     def training_step(self, batch, batch_idx):
         outputs = self.forward(
@@ -46,13 +54,49 @@ class EOIRModel(pl.LightningModule):
         return outputs.loss
 
     def validation_step(self, batch, batch_idx):
-        logits = self.forward(batch["input_ids"], batch["attention_mask"])
-        loss = F.cross_entropy(logits, batch["label"])
-        _, preds = torch.max(logits, dim=1)
-        val_acc = accuracy_score(preds.cpu(), batch["label"].cpu())
-        val_acc = torch.tensor(val_acc)
-        self.log("val_loss", loss, prog_bar=True)
-        self.log("val_acc", val_acc, prog_bar=True)
+        labels = batch["label"]
+        outputs = self.forward(
+            batch["input_ids"], batch["attention_mask"], labels=batch["label"]
+        )
+        preds = torch.argmax(outputs.logits, 1)
+
+        # Metrics
+        valid_acc       = self.val_accuracy_metric(preds, labels)
+        precision_macro = self.precision_macro_metric(preds, labels)
+        recall_macro    = self.recall_macro_metric(preds, labels)
+        precision_micro = self.precision_micro_metric(preds, labels)
+        recall_micro    = self.recall_micro_metric(preds, labels)
+        f1              = self.f1_metric(preds, labels)
+
+        # Logging metrics
+        self.log("valid/loss", outputs.loss, prog_bar=True, on_step=True)
+        self.log("valid/acc", valid_acc, prog_bar=True)
+        self.log("valid/precision_macro", precision_macro, prog_bar=True)
+        self.log("valid/recall_macro", recall_macro, prog_bar=True)
+        self.log("valid/precision_micro", precision_micro, prog_bar=True)
+        self.log("valid/recall_micro", recall_micro, prog_bar=True)
+        self.log("valid/f1", f1, prog_bar=True)
+        return {"labels": labels, "logits": outputs.logits}
+    
+    def validation_epoch_end(self, outputs):
+        labels = torch.cat([x["labels"] for x in outputs])
+        logits = torch.cat([x["logits"] for x in outputs])
+        preds = torch.argmax(logits, 1)
+
+
+        self.logger.experiment.log(
+            {
+                "conf": wandb.plot.confusion_matrix(
+                    probs=logits.cpu().numpy(), 
+                    y_true=labels.cpu().numpy()
+                )
+            }
+        )
+        # wandb.log({"cm": wandb.sklearn.plot_confusion_matrix(
+        #     labels.detach().cpu().clone().numpy(), 
+        #     preds.detach().cpu().clone().numpy())}
+        # )
+
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams["lr"])
--- src/train.py
+++ src/train.py
@@ -1,13 +1,38 @@
-import torch
+import torch, wandb
 import pytorch_lightning as pl
+import pandas as pd
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 
-wandb_logger = WandbLogger(project="nlp_app")
-
 from data import DataModule
 from model import EOIRModel
+
+class SamplesVisualisationLogger(pl.Callback):
+    def __init__(self, datamodule):
+        super().__init__()
+
+        self.datamodule = datamodule
+
+    def on_validation_end(self, trainer, pl_module):
+        val_batch = next(iter(self.datamodule.val_dataloader()))
+        sentences = val_batch["text"]
+
+        outputs = pl_module(val_batch["input_ids"], val_batch["attention_mask"])
+        preds = torch.argmax(outputs.logits, 1)
+        labels = val_batch["label"]
+
+        df = pd.DataFrame(
+            {"Sentence": sentences, "Label": labels.numpy(), "Predicted": preds.numpy()}
+        )
+
+        wrong_df = df[df["Label"] != df["Predicted"]]
+        trainer.logger.experiment.log(
+            {
+                "examples": wandb.Table(dataframe=wrong_df, allow_mixed_types=True),
+                "global_step": trainer.global_step,
+            }
+        )
 
 
 def main():
@@ -15,22 +40,32 @@ def main():
     eoir_model = EOIRModel()
 
     checkpoint_callback = ModelCheckpoint(
-        dirpath="./models", monitor="val_loss", mode="min"
-    )
-    early_stopping_callback = EarlyStopping(
-        monitor="val_loss", patience=3, verbose=True, mode="min"
+        dirpath="./models",
+        filename="best-checkpoint",
+        monitor="valid/loss",
+        mode="min",
     )
 
+    early_stopping_callback = EarlyStopping(
+        monitor="valid/loss", patience=3, verbose=True, mode="min"
+    )
+
+    wandb_logger = WandbLogger(project="nlp_app", entity="ramonpzg")
+    
     trainer = pl.Trainer(
-        default_root_dir="logs",
-        gpus=(1 if torch.cuda.is_available() else 0),
-        max_epochs=5,
-        fast_dev_run=False,
+        max_epochs=2,
         logger=wandb_logger,
-        callbacks=[checkpoint_callback, early_stopping_callback],
+        callbacks=[checkpoint_callback, SamplesVisualisationLogger(eoir_data), early_stopping_callback],
+        log_every_n_steps=10,
+        deterministic=True,
+        # gpus=(1 if torch.cuda.is_available() else 0),
+        # accelerator='gpu',
+        # devices=1
+        # limit_train_batches=0.25,
+        # limit_val_batches=0.25
     )
     trainer.fit(eoir_model, eoir_data)
-
+    wandb.finish()
 
 if __name__ == "__main__":
     main()
